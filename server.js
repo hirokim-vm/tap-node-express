423156a473c3eebd678e1b8bfe398856390ae1a7
unknown <piroki.world.20100411@gmail.com>
2022-11-26 01:26:08 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server 20221126!');
+});
+
+app.get('/health', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
